76f160a40201d8b05789f9949923074259a81f1f
Felipe Mesquita <felimc90@hotmail.com>
2022-09-09 23:21:41 -0400

    quelques corrections aux tests

--- src/Pile.java
+++ src/Pile.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pile {
+    private boolean _vide = true;
+    private List<Integer> _listeElement = new ArrayList<Integer>();
 
     public boolean estVide() {
-        return true;
+        return _vide;
+    }
+
+    public void empiler(int element) {
+        _listeElement.add(element);
+        _vide = false;
+    }
+
+    public int depiler() {
+        int index = (_listeElement.size() - 1);
+        int dernierElement = _listeElement.get(index);
+        _listeElement.remove(index);
+        return dernierElement;
     }
 }
--- src/PileTest.java
+++ src/PileTest.java
@@ -4,8 +4,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PileTest {
     @Test
-    public void pile_InitialementEstVide(){
+    public void tester_Pile_InitialementEstVide(){
         Pile pile = new Pile();
         assertTrue(pile.estVide());
+    }
+
+    @Test
+    public void tester_Empilage_AvecUnElement_PileNestPlusVide(){
+        Pile pile = new Pile();
+        pile.empiler(5);
+        assertFalse(pile.estVide());
+    }
+
+    @Test
+    public void tester_EmpilageEtDepilage_AvecUnElement_DoitRetournerElementDepile(){
+        int element = 5;
+        Pile pile = new Pile();
+        pile.empiler(element);
+        int elementRetourne = pile.depiler();
+
+        assertEquals(element, elementRetourne);
+    }
+
+    @Test
+    public void tester_EmpilageEtDepilage_AvecPlusieursElements_DoitDepilerOrdreInverse() {
+        Pile pile = new Pile();
+        pile.empiler(25);
+        pile.empiler(5);
+
+        int premierDepile = pile.depiler();
+        int deuxiemeDepile = pile.depiler();
+
+        assertEquals(premierDepile, 5);
+        assertEquals(deuxiemeDepile,25);
     }
 }
